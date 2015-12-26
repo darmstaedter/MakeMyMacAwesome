d2cf2854854914f8c2b5760d93b98e06c0bf3247
Frederic Darmstädter <frederic@darmstaedter.org>
2015-12-26 18:13:42 +0100

    Software added

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+## XCode Commandline Tools
+xcode-select --install
+
 ## Install Homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
@@ -15,3 +18,26 @@ rbenv global 2.2.3
 ## Install rails 4.2.4
 gem install rails -v 4.2.4
 rbenv rehash
+
+## Install Homebrew Cask
+brew install caskroom/cask/brew-cask
+
+## Install Software
+brew install git
+brew install nodejs
+brew cask install google-chrome
+brew cask install postgres
+brew cask install skype
+brew cask install slack
+brew cask install sketch sketch-tool sketch-toolbox
+brew cask install tower
+brew cask install transmit
+brew cask install sequel-pro
+brew cask install iterm2
+brew cask install firefox
+brew cask install vlc
+brew cask install mplayerx
+brew cask install coda
+brew cask install atom
+brew cask install keka
+brew cask install adobe-creative-cloud
