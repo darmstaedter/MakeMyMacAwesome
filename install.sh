80c04caea6d847794bf9f481498f4f527b33ca33
Frederic Darmstädter <frederic@darmstaedter.org>
2016-02-16 15:59:07 +0100

    Update Ruby Version

--- install.sh
+++ install.sh
@@ -6,14 +6,14 @@ xcode-select --install
 ## Install Homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-## Install ruby 2.2.3
+## Install ruby 2.3.0
 brew install rbenv ruby-build
 
 echo 'if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi' >> ~/.bash_profile
 source ~/.bash_profile
 
-rbenv install 2.2.3
-rbenv global 2.2.3
+rbenv install 2.3.0
+rbenv global 2.3.0
 
 ## Install rails 4.2.4
 gem install rails -v 4.2.4
